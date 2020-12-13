217b2fb4b64d678774ecd56b11627c75a4667652
cgretzem <64376353+cgretzem@users.noreply.github.com>
2020-12-12 23:21:02 -0700

    Fully documented all methods, added champ select capability.
    Fixed an error where data was being sent to server as dictionary instead of JSON

--- Client_interface.py
+++ Client_interface.py
@@ -1,46 +1,76 @@
 import subprocess
 import pprint
-import asyncio
 import requests
 import base64
+import json
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 """
 Handles classes and methods for an active local LOL Client.
+
 Classes:
+----------------------
     `Client` - Represents an active LOL client.
 """
 class Client():
     """
     A class to represent an active LOL client.
+
     Attributes:
     ----------
     `session` : Session 
         A requests.Session() object loaded with the login info for the LCU.
     `app_port` : str
         The port that the LCU chooses to connect to. Changes every time the client is launched.
+
     Methods:
     ----------
     `check_connection()` : bool 
         Checks the connection to the client by retrieving current summoner at `/lol-summoner/v1/current-summoner`.
+
     `get_rune_pages()` : dict
         Retrieves the list of rune pages for the current user.
+
     `get_current_page()` : dict
         Retrives the currently active rune page details.
+
+    `get_pickable_champions()` : list
+        Returns list of pickable champions.
+
+    `change_current_page`(`name` : str, `primary_tree` : int, `perks` : list, `secondary_tree` : int)
+        Changes runes and name of a rune page.
+
+    `change_summoners`(`spell_1` : int, `spell_2` : int) : Response
+        Changes the summoner spells to the new summoner IDs. Returns response code.
+    
+    `get_game_phase()` : str
+        Returns the current game phase.
+
+    select_champ(`champID`: int) : bool
+        Locks in the champion with the specified ID. Returns true if lock was successful.
+
     `get_req( str )` : Response
         Auxillary function to aid in making get requests with localhost and app port already filled into the url.
+
     `patch_req( str )` : Response
         Auxillary function to aid in making patch requests with localhost and app port already filled into the url.
+
     `put_req( str )` : Response
         Auxillary function to aid in making put requests with localhost and app port already filled into the url.
+
     `post_req( str )` : Response
         Auxillary function to aid in making post requests with localhost and app port already filled into the url.
+
     `__login()` : None
         Private method to be run in initalization of Client object. Initalizes session object and connects to LCU.
     """
     def __init__(self):
         """
-        Constructor for Client Object, connects to LCU API
+        Constructor for Client Object, connects to LCU API.
+
+        Returns : 
+        -------------
+        `Client` 
         """
         self.session = None
         self.app_port = ""
@@ -52,6 +82,7 @@ class Client():
     def check_connection(self):
         """
         Method to check if a sucessfull connection was established with the LCU API
+
         Returns
         -----------------
         `bool` : True when the connection is successful
@@ -60,10 +91,23 @@ class Client():
         self.summoner_id = resp.json()['summonerId']
         return resp.ok
 
-        
+    
+    def get_pickable_champions(self):
+        """
+        Method to get all the pickable champion IDs for a given champ select.
+
+        Returns:
+        -----------------
+        `list` : full of champion IDs that can be picked
+        """
+
+        return self.get_req('/lol-champ-select/v1/pickable-champion-ids').json()
+
+    
     def get_rune_pages(self):
         """
         Method to get all rune pages.
+
         Returns
         -----------------
         `dict` : All availible rune page details, including default rune pages.
@@ -71,17 +115,29 @@ class Client():
         Contents of dict:
         -----------------------------------
         `autoModifiedSelections` : list
+
         `current` : bool
+
         `id` : float
+
         `isActive` : bool
+
         `isDeletable` : bool
+
         `isEditable` : bool
+
         `isValid` : bool
+
         `lastModified` : float
+
         `name` : str
+
         `order` : int
+
         `primaryStyleId` : int
+
         `selectedPerkIds : list(int)
+
         `subStyleId` : int
         
         """
@@ -95,20 +151,33 @@ class Client():
         Returns
         -----------------
         `dict` : dictionary with runepage details.
+
         Contents of dict:
         -----------------------------------
         `autoModifiedSelections` : []
+
         `current` : bool
+
         `id` : float
+
         `isActive` : bool
+
         `isDeletable` : bool
+
         `isEditable` : bool
+
         `isValid` : bool
+
         `lastModified` : float
+
         `name` : str
+
         `order` : int
+
         `primaryStyleId` : int
+
         `selectedPerkIds : [int]
+
         `subStyleId` : int
         
         """
@@ -118,16 +187,24 @@ class Client():
     def change_current_page(self, name, primary_tree : int, perks : list, secondary_tree : int):
         """
         Method to change the current runepage to the specified new data.
+
         Parameters:
         -----------
         name : str
             The new name of the rune page.
+
         primary_tree : int
             The ID of the new primary tree.
+
         perks : list
             List of the intended perk IDs. Must have a length of 9.
+
         secondary_tree : int
             ID of the secondary perk tree.     
+
+        Returns : 
+        --------------------
+        `Response` : the response object from the post request.
         """
         data = self.get_current_page()
         data["name"] = name
@@ -139,12 +216,14 @@ class Client():
     def change_summoners(self, spell_1 : int, spell_2 : int):
         """
         Method to change the current summoner spells to new ones.
+
         Parameters :
         -------------
         `spell_1` : int
             ID code for the left summoner spell.
         `spell_2` : int
             ID code for the right summoner spell.
+
         Returns : 
         -------------
         `Response` : Response
@@ -154,33 +233,60 @@ class Client():
             "spell1Id": spell_1,
             "spell2Id": spell_2
         }
-        self.patch_req('/lol-champ-select/v1/session/my-selection', data = data)
+        return self.patch_req('/lol-champ-select/v1/session/my-selection', data = json.dumps(data))
 
 
     def get_champ_select(self):
+        """
+        Method to return all data from a given champ select.
+
+        Returns : 
+        --------------------
+        `dict` : all data from champ select
+        """
         return self.get_req('/lol-champ-select/v1/session').json()
     
-    def in_champ_select(self):
-        return self.get_req('/lol-champ-select/v1/session').ok
+    def get_game_phase(self):
+        """
+        Method to return the current game phase.
 
-    async def select_champ(self, champID: int):
+        Returns : 
+        -------------
+        `str` : current game phase
+        """
+        return self.get_req('/lol-gameflow/v1/gameflow-phase').json()
+
+    def select_champ(self, champID: int):
+        """
+        Method to lock in the champion with the specified ID.
+
+        Parameters : 
+        -------------------
+        `ChampID` : int
+            ID that corresponds to the champion to be locked in.
+
+        Returns : 
+        ----------------
+        `bool` : True if both the hover operation and the lock operation return code 200.
+        """
         cellID = -1
-        ID = -1
-        data = {}
+        playerID = -1
         select = self.get_champ_select()
         team = select['myTeam']
+
         for player in team:
-            if(team['summonerId'] == self.summoner_id):
-                cellID = team['cellId']
-        for player in select['actions'][0][0]:
+            if(player['summonerId'] == self.summoner_id):
+                cellID = player['cellId']
+
+        for player in select['actions'][0]:
             if player['actorCellId'] == cellID:
-                ID = player['id']
-                data = player
-        data['championId'] = champID
-        self.patch_req(f'/lol-champ-select/v1/session/actions/{ID}', data = data)
-        asyncio.sleep(1)
-        data['completed'] = True
-        self.patch_req(f'/lol-champ-select/v1/session/actions/{ID}', data = data)
+                playerID = player['id']
+                print(type(playerID))
+        data = {'championId': champID,}
+
+        hover = self.patch_req(f'/lol-champ-select/v1/session/actions/{playerID}', data = json.dumps(data))
+        lock = self.post_req(f'/lol-champ-select/v1/session/actions/{playerID}/complete', data = json.dumps(data))
+        return (hover.ok and lock.ok)
     def __login(self):
         """
         Method to be called in the constructor of Client in order to interface with the LCU API.
@@ -277,8 +383,3 @@ class Client():
         """
         return self.session.get('https://127.0.0.1:' + self.app_port + endpoint, verify = False)
     
-if __name__ == "__main__":
-    client = Client()
-    print(client.check_connection())
-    print(client.summoner_id)
-    pprint.pprint(client.select_champion(1,1))
